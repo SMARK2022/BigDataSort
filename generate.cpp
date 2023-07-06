1e7a5baa19759eaffe2a32ae4fef25b727a50b14
SMARK <3222301373@qq.com>
2023-07-06 19:42:07 +0800

    一堆代码和样例，加油同志们

--- generate.cpp
+++ generate.cpp
@@ -1,48 +1,95 @@
 #include <iostream>
-#include <fstream>
+#include <stdio.h>
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <thread>
+#include <vector>
+#include <mutex>
 
-std::string generateRandomString(int length)
+std::mutex mtx;
+FILE *outputFile;
+
+typedef struct Str
 {
-    std::string randomString = "";
+    char character[16];
+} Str;
+
+Str Str_NULL = {'\0'};
+
+Str generateRandomString(int length)
+{
+    Str randomString;
     static const char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 
     for (int i = 0; i < length; i++)
     {
         int index = rand() % (sizeof(alphabet) - 1);
-        randomString += alphabet[index];
+        randomString.character[i] = alphabet[index];
     }
-
+    randomString.character[length] = '\n';
     return randomString;
+}
+
+void generateAndWriteRandomStrings(int numLines, int stringLength, int threadId)
+{
+    int batchSize = 10485760;
+    int numBatches = (numLines + batchSize - 1) / batchSize;
+
+    for (int batch = 0; batch < numBatches; batch++)
+    {
+        int linesToGenerate = std::min(batchSize, numLines - batch * batchSize);
+
+        // Create a vector to store the lines
+        Str *lines = (Str *)malloc((linesToGenerate + 1) * sizeof(Str));
+
+        for (int i = 0; i < linesToGenerate; i++)
+        {
+            // Add the random string to the vector
+            lines[i] = generateRandomString(stringLength);
+        }
+        lines[linesToGenerate] = Str_NULL;
+
+        // Lock the mutex before writing to the file
+        mtx.lock(); // 不加锁就注释掉
+        std::cout << "Writing " << linesToGenerate * 16 / 1024 / 1024 << "MB";
+
+        // Write the lines to file
+        // std::cout << lines[0].character;
+        fprintf(outputFile, lines[0].character);
+        free(lines);
+        std::cout << " OK" << std::endl;
+        mtx.unlock(); // 不加锁就注释掉
+    }
 }
 
 int main()
 {
     srand(time(0));
 
-    std::ofstream outputFile("DATA.txt");
-    if (!outputFile)
+    if (!(outputFile = fopen("DATA16G.txt", "w")))
     {
         std::cerr << "Failed to open file." << std::endl;
         return 1;
     }
 
-    long long numLines = 1000000;
-    int epoch = 100;
+    int numLines = 1610612736;
     int stringLength = 15;
-    for (int j = 0; j < epoch; j++)
+    int numThreads = std::thread::hardware_concurrency(); // Get the number of hardware threads
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < numThreads; i++)
     {
-        for (long long i = 0; i < numLines; i++)
-        {
-            std::string randomString = generateRandomString(stringLength);
-            outputFile << randomString << std::endl;
-        }
-        std::cout << "Epoch: " << j << "*10^6" << std::endl;
+        threads.emplace_back(generateAndWriteRandomStrings, numLines / numThreads, stringLength, i);
     }
 
-    outputFile.close();
+    // Wait for all threads to finish
+    for (auto &thread : threads)
+    {
+        thread.join();
+    }
+
+    fclose(outputFile);
 
     return 0;
 }
